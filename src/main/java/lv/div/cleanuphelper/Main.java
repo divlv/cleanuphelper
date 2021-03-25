2eaf72433e78c227e8a739833e03d9228836e21a
dimedrol <mail@me.com>
2021-03-25 15:01:30 +0200

    Initial files

---
+++ src/main/java/lv/div/cleanuphelper/Main.java
@@ -0,0 +1,11 @@
+package lv.div.cleanuphelper;
+
+import java.util.Date;
+/**
+ * Main starting point.
+ */
+public class Main {
+	public static void main(String[] args) throws Exception {
+		System.out.println( "App is working! Current time is " + new Date());
+		}
+}
